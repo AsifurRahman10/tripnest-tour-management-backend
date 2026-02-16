62aaf71e5adc1dc0a33e95f5c6dd38b1f13dc661
Asifur Rahman <asifur.rahman@testsignal.com>
2026-02-16 20:24:18 +0600

    password hashing

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,11 +1,26 @@
-import { IUser } from './user.interface'
+import httpStatusCode from 'http-status-codes'
+import AppError from '../../errorHelpers/AppError'
+import { IAuthProvider, IUser } from './user.interface'
 import { User } from './user.model'
+import bcrypt from 'bcryptjs'
 
 const createUserService = async (payload: Partial<IUser>) => {
-  const { name, email } = payload
+  const { email, password, ...rest } = payload
+  const isUserExist = await User.findOne({ email })
+  if (isUserExist) {
+    throw new AppError(httpStatusCode.FORBIDDEN, 'User already exist')
+  }
+  const hashPassword = await bcrypt.hash(password as string, 8)
+
+  const authProvider: IAuthProvider = {
+    provider: 'credential',
+    providerID: email as string,
+  }
   const user = await User.create({
-    name,
     email,
+    password: hashPassword,
+    ...rest,
+    auths: [authProvider],
   })
 
   return user
