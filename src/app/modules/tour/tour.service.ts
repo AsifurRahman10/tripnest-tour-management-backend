9797d8c9d3b6f1446d1c981da30e6f5d32334536
Asifur Rahman <asifur.rahman4904@gmail.com>
2026-04-15 13:25:07 +0600

    Refactor tour interfaces: rename and relocate ITour and ITourType to xtour.interface.ts; update import paths in model and service files

--- src/app/modules/tour/tour.service.ts
+++ src/app/modules/tour/tour.service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { ITour, ITourType } from './Tour.interface'
+import { ITour, ITourType } from './xtour.interface'
 import { TourType, Tour } from './tour.model'
 
 const createTourTypes = async (data: ITourType) => {
